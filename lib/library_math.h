e87070dbc2140ad997c1427fbb3ae4e18ce49ec4
Dmitry <dmitry@example.com>
2025-12-24 05:50:44 +0000

    Current clang-format config

--- lib/library_math.h
+++ lib/library_math.h
@@ -1,16 +1,15 @@
 #pragma once
 
-namespace library_math
-{
-    double sum(double num1, double num2);
+namespace library_math {
+double sum(double num1, double num2);
 
-    double minus(double num1, double num2);
+double minus(double num1, double num2);
 
-    double multiplication(double num1, double num2);
+double multiplication(double num1, double num2);
 
-    double division(double num1, double num2);
+double division(double num1, double num2);
 
-    double exponentiation(double num, double degree);
+double exponentiation(double num, double degree);
 
-    double factorial(double num);
-}
+double factorial(double num);
+} // namespace library_math
